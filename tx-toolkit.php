b341148a5593f3abf35509d11c8c2809e15e2767
marsian <marsian@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 18:30:37 +0000

    10th commit of plugin templatesnexttoolkit
    git-svn-id: https://plugins.svn.wordpress.org/templatesnext-toolkit/trunk@1092678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tx-toolkit.php
+++ tx-toolkit.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: TemplatesNext ToolKit
 Description: Custom Portfolio and Shortcode functionality for TemplatesNext Wordpress Themes
-Version: 1.0.9
+Version: 1.1.0
 Author: TemplatesNext
 Author URI: http://templatesnext.org/
 License: GPLv2 or later
